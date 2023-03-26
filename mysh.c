268dad0be8b216b9d4578ea2d53cb83ccc2dfd6c
joaorobiatti <joao.robiatti@gmail.com>
2023-03-26 18:31:08 -0300

    Formatação da cadeia de entrada e atualização gitignore

--- mysh.c
+++ mysh.c
@@ -14,6 +14,23 @@ void type_prompt(){
 	printf("[MySh] nome-de-usuario@hospedeiro:diretorio-atual$ ");
 }
 
+char** format_text_line(char *text){
+	int i = 0;
+	char* word;
+	char** args_list = (char **) malloc(sizeof(char*) * 20); 
+	const char delimiter[2] = " ";
+	//separa utilizando o delimitador " ", pegando o primeiro token
+	word = strtok(text, delimiter);
+	
+	while (word != NULL) {
+		args_list[i] = strdup(word);
+		i++;
+		word = strtok(NULL, delimiter);
+	}
+	args_list[i] = NULL;
+
+	return args_list;
+}
 
 pid_t spawn(char* program, char** arglist){
 	pid_t child_pid = fork();
@@ -31,8 +48,9 @@ pid_t spawn(char* program, char** arglist){
 int main(int argc, char* argv){
 	
 	char text_line[LINE_SIZE];
-	char *test[] ={"ls", "-l", NULL};
+	//char *test[] ={"ls", "-l", NULL};
 	int child_status;
+	int i;
 
 	while(true){ 
 		// printa a config inicial do prompt
@@ -40,22 +58,33 @@ int main(int argc, char* argv){
 		// recebe o comando digitado pelo usuario
 		fgets(text_line, LINE_SIZE, stdin);
 		// printa o comando digitado
-
 		text_line[strlen(text_line)-1] = '\0';
 
-		// executa o comando ls
-		spawn("ls", test); // ! deixar isso generico
+		char** args_list = format_text_line(text_line);
 
-		// espera o processo filho encerrar
-		wait(&child_status);
-		if(!WIFEXITED(child_status)){
-			printf("O processo filho terminou anormalmente\n");
+		if(strcmp(args_list[0], "help") == 0){
+			//print_usage
 		}
-
-		// sai do programa se receber o comando exit
-		if(strcmp(text_line, "exit") == 0){
+		else if(strcmp(args_list[0], "cd") == 0){
+			//change_directory(params)  
+		}
+		else if(strcmp(args_list[0], "exit") == 0){ // sai do programa se receber o comando exit
 			break;
 		}	
+		else{ // comando externo
+			spawn(args_list[0], args_list); 
+
+			// espera o processo filho encerrar
+			wait(&child_status);
+			if(!WIFEXITED(child_status)){
+				printf("O processo filho terminou anormalmente\n");
+			}
+		}
+		
+		for (i=0; args_list[i] != NULL; i++) {
+    		free(args_list[i]);
+		}
+		free(args_list);
 	}
 
 	return 0;
