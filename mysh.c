7a12c5b47087abba35b73bddf5bd8043208d33f9
m-missano <murilombell@gmail.com>
2023-03-26 11:25:18 -0300

    documentation

--- mysh.c
+++ mysh.c
@@ -4,24 +4,27 @@
 #include <sys/types.h>
 #include <string.h>
 
+#define LINE_SIZE 10000
+
 void type_prompt(){
+	// ! verificar como pegar o nome do usuario
 	printf("[MySh] nome-de-usuario@hospedeiro:diretorio-atual$");
 }
 
 int main(int argc, char* argv){
 	
-	char* text_line;
-	size_t line_size;
+	char text_line[LINE_SIZE];
 	
-	while(true){
-		type_prompt();
-	        getline(&text_line, &line_size, stdin);
-		//fgets(text_line);
-		printf("%s\n", text_line);
-		printf("%d\n", line_size);
+	while(true){ 
+		// printa a config inicial do prompt
+		type_prompt();	       
+		// recebe o comando digitado pelo usuario
+		fgets(text_line, LINE_SIZE, stdin);
+		// printa o comando digitado
+		printf("%s\n", text_line); // ! executar o comando
+		// sai do programa se receber o comando exit
 		if(strcmp(text_line, "exit\n") == 0){
 			break;
 		}	
 	}
-	free(text_line);
 }
